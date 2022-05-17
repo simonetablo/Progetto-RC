88b0dee7982d1bdde483b2422f81b15ca09c3914
simonetablo <simone.tablo@gmail.com>
2022-05-17 14:56:40 +0200

    avanzamento frontend. inoltro richieste backend

--- server/app/app.js
+++ server/app/app.js
@@ -1,10 +1,12 @@
-   var OpenTripMapKey = "5ae2e3f221c38a28845f05b6e8cfaa33e6a2f1fbe1d1350f053db399";
 
-    mapboxgl.accessToken = "pk.eyJ1Ijoic2ltb25ldGFibG8iLCJhIjoiY2wzMXFvYW0xMDI0ZjNjb2ZmOGx5eWMzMSJ9.D_d2l01EuXlPcVxIdhaRww";
+   var OpenTripMapKey = "5ae2e3f221c38a28845f05b6e8cfaa33e6a2f1fbe1d1350f053db399";
+   var mapBoxAT="pk.eyJ1Ijoic2ltb25ldGFibG8iLCJhIjoiY2wzMXFvYW0xMDI0ZjNjb2ZmOGx5eWMzMSJ9.D_d2l01EuXlPcVxIdhaRww"
+
+    mapboxgl.accessToken = mapBoxAT;
     var map = new mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/mapbox/streets-v11",
-        center: [-122.32, 47.56],
+        center: [12.5, 41.9],
         zoom: 8
     });
 
@@ -55,4 +57,95 @@
                 map.setPaintProperty("OTM-pois-"+name, 'circle-color', "rgb(225,97,237)")
             }
         }
+
+        function APIreq(xid) {
+            return new Promise(function (resolve, reject) {
+                var url ="https://api.opentripmap.com/0.1/en/places/xid/"+xid+"?apikey="+OpenTripMapKey;
+                fetch(url)
+                    .then(response => response.json())
+                    .then(data => resolve(data))
+                    .catch(function (err) {
+                        console.log("Fetch Error :-S", err);
+                    });
+            });
+        }
+    
+        function showInfo(data, lngLat) {
+            let poi = document.createElement("div");
+            poi.innerHTML = "<h2>" + data.name + "<h2>";
+            poi.innerHTML += "<p><i>" + getCategoryName(data.kinds) + "</i></p>";
+            if (data.preview) {
+                poi.innerHTML += "<img src='"+data.preview.source+"'>";
+            }
+            poi.innerHTML += data.wikipedia_extracts
+                ? data.wikipedia_extracts.html
+                : data.info
+                    ? data.info.descr
+                    : "No description";
+    
+            poi.innerHTML += "<p><a target='_blank' href='"+ data.otm + "'>Show more at OpenTripMap</a></p>";
+            
+            var info=document.getElementById('info');
+            info.innerHTML="";
+            info.appendChild(poi);
+
+        }
+
+        /*map.on("click", "OTM-pois-"+name, function(e) {
+            let coordinates = e.features[0].geometry.coordinates.slice();
+            let id = e.features[0].properties.id;
+            let poiname = e.features[0].properties.name;
+            APIreq(id).then(data => showInfo(data, e.lngLat));
+        });
+        */
+        map.on("click", "OTM-pois-"+name, function(e) {
+            let coordinates = e.features[0].geometry.coordinates.slice();
+            let id = e.features[0].properties.id;
+            let poiname = e.features[0].properties.name;
+            APIreq(id).then(data => showInfo(data, e.lngLat));
+            var data={
+                name:poiname
+            }
+            var dataString=JSON.stringify(data);
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:3000/data",
+                dataType: "json",
+                data: {
+                    info: dataString
+                },
+                success: function(data) {
+                    alert('success')
+                },
+                error: function() {
+                    alert('error')
+                }
+            })
+
+        });
+        
+
+        let popup = new mapboxgl.Popup({
+            closeButton: false,
+            closeOnClick: false,
+        });
+
+        map.on("mouseenter", "OTM-pois-"+name, function (e) {
+            map.getCanvas().style.cursor = "pointer";
+            let coordinates = e.features[0].geometry.coordinates.slice();
+            let id = e.features[0].properties.id;
+            let poiname = e.features[0].properties.name;
+            popup
+                .setLngLat(coordinates)
+                .setHTML("<strong>" + poiname + "</strong>")
+                .addTo(map);
+        });
+
+        map.on("mouseleave", "OTM-pois-"+name, function () {
+            map.getCanvas().style.cursor = "";
+            popup.remove();
+        });   
+
     }
+
+
--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 
 var app = express();
+app.use(express.static('app'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
-  res.sendFile('./app/index.html', {root: __dirname});
+  res.sendFile('/app/index.html', {root: __dirname});
+});
+
+app.post('/data', function(req, res){
+  var data = JSON.parse(req.body.info);
+  console.log(data.name);
 });
 
 app.listen(3000);
