0216abc1aefcf5f71cfce20227a058270133e351
simonetablo <simone.tablo@gmail.com>
2022-05-21 18:34:33 +0200

    richiesta coordinate localita' inserita nella form

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const oAuthSet = require ('./middlewares/oAuthSet')
 
 
 var app = express();
-//app.use(express.static('app'));
+app.use(express.static('app'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var OpenTripMapKey = "5ae2e3f221c38a28845f05b6e8cfaa33e6a2f1fbe1d1350f053db399";
@@ -20,10 +20,28 @@ app.get('/', function(req, res){
 });
 
 app.post('/', function(req, res){
-//  dati in req.body 
-  console.log(req.body);
+  var inizio_viaggio=req.body.inizio_viaggio;
+  var fine_viaggio=req.body.fine_viaggio;
+  var localita=req.body.localita;
+  console.log(inizio_viaggio);
+  console.log(fine_viaggio);
+  console.log(localita);
+  request({
+    url:"https://api.opentripmap.com/0.1/en/places/geoname?name="+localita+"&apikey="+OpenTripMapKey,
+    method: "GET",
+  },
+  function(error, response, body){
+    if(error) {
+      console.log(error);
+    } else if (!error && response.statusCode==200){
+      var info=JSON.parse(body);
+      var lat=info.lat;
+      var lon=info.lon;
+      console.log(lon+", "+lat);
+    }
+  })
   
-  res.sendFile('./app/index.html', {root: __dirname} )
+  res.sendFile('./app/planner.html', {root: __dirname} )
 });
 
 
